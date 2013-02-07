8ea86b2c01b78a5274ca8e8b14387a6f63b1d118
Brian Pappas <bjp0001@auburn.edu>
2013-02-07 15:43:56 -0600

    Started Coding Reversi.  Working on detecting valid moves

--- reversi/reversi.cpp
+++ reversi/reversi.cpp
@@ -1,28 +1,80 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool validMove(char player, int x, int y, vector <string> board)
+{	
+	char oppPlayer;
+	//Determine opposite player
+	if(player = 'W')
+		oppPlayer = 'B';
+	else
+		oppPlayer = 'W';
+		
+	//If not empty space, invalid move
+	if(board[x][y] != '-')
+	{
+		return false;
+	}
+		
+	//If a neighbor is not opposite player, invalid move
+	bool neighbor = false;	
+	for(int i=-1;i<=1;i++)
+	{
+		for(int j=-1;j<=1;j++)
+		{		 
+			int testX = x+i;
+			int testY = y+j;
+			if(testX>=0 && testX<8 && testY>=0 && testY<8)
+			{
+				if(board[testX][testY] == oppPlayer)
+					return true;
+			}
+		}
+	}	
+	
+	return false;
+}
 
 /* Head ends here */
-void nextMove(char player, char board[9][9])
+void nextMove(char player, vector <string> board)
 {
-    //logic here
+    for (int i=0;i<8;i++)
+    {
+    	for (int j=0;j<8;j++)
+    	{
+    		if(validMove(player, i,j,board))
+    		{
+    			board[i][j]='V';
+    			cerr << endl;
+    		}
+    	}
+    }
+    
+    //DEBUG PRINT BARD
+	for (int m=0;m<8;m++)
+	{
+		cerr << board[m] << endl;
+	}
 }
+
 
 /* Tail starts here */
 int main() 
 {
 
-    int i;
     char player;
-    char board[9][9];
-    char line[9];
-    scanf("%c", &player);
+    vector <string> board;
 
-    for(i=0; i<9; i++) 
+    cin >> player;
+
+    for(int i=0; i<8; i++) 
     {
-        scanf("%s", line);
-        strncpy(board[i], line, 9);
+        string s; cin >> s;
+        board.push_back(s);
     }
+  
     nextMove(player,board);
+
     return 0;
 }
